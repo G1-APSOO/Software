fdf16dda9b2a0d1269184a25643c51487c908c60
Matheus <46912481+Mat780@users.noreply.github.com>
2023-10-22 20:39:02 -0400

    build: Implementa a classe OrcamentoLocacaoDeEspaco

--- src/classes/OrcamentoLocacaoDeEspaco.java
+++ src/classes/OrcamentoLocacaoDeEspaco.java
@@ -1,5 +1,12 @@
-package Classes;
+package classes;
 
-public class OrcamentoLocacaoDeEspaco {
-
+public class OrcamentoLocacaoDeEspaco extends OrcamentoEvento {
+	
+	public OrcamentoLocacaoDeEspaco(int numeroDeConvidados, int numeroDeColaboradores, String horaDeInicio) {
+		super(numeroDeConvidados, numeroDeColaboradores, horaDeInicio);
+	}
+	
+	public OrcamentoLocacaoDeEspaco(int numeroDeConvidados, String horaDeInicio) {
+		super(numeroDeConvidados, horaDeInicio);
+	}
 }
