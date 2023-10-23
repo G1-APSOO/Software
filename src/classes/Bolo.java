f17e2258d86e88a2b09a29369f3dc1908f5f9d32
Matheus <46912481+Mat780@users.noreply.github.com>
2023-10-22 20:36:31 -0400

    build: Implementa classe bolo

--- src/classes/Bolo.java
+++ src/classes/Bolo.java
@@ -1,26 +1,28 @@
-package Classes;
+package classes;
+
+import java.util.ArrayList;
 
 public class Bolo extends Alimento {
 	private Double peso;
 	
-	public Bolo(Double peso, int id, String descricao, double valorUnitario) {
+	public Bolo(int id, String descricao, double valorUnitario) {
 		super(id, descricao, valorUnitario);
-		setPeso(peso);
 	}
 
-	private void setPeso(Double peso) {
+	public Double getValor() {
+		return getValorUnitario() * peso;
+	}
+	
+	public static ArrayList<Bolo> getAllBolos() {
+		return BoloDAO.getAll();
+	}
+	
+	public void setPeso(Double peso) {
 		this.peso = peso;
 	}
 	
 	public Double getPeso() {
 		return peso;
 	}
-	
-	public Double getValor(int id) {
-		Double valorBolo;
-		
-		valorBolo = getValorUnitario() * peso;
-		return valorBolo;
-	}
-	
+
 }
