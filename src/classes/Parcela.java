7dc276ee50c24bd9d7bd3f0d86bcdb1738be9068
Matheus <46912481+Mat780@users.noreply.github.com>
2023-10-22 20:39:14 -0400

    build: Implementa a classe Parcela

--- src/classes/Parcela.java
+++ src/classes/Parcela.java
@@ -1,5 +1,37 @@
-package Classes;
+package classes;
 
 public class Parcela {
-
+	private Double valorPago;
+	private Data dataVencimento;
+	private Data dataPagamento;
+	
+	public Parcela(Double valorPago, Data dataVencimento, Data dataPagamento) {
+		setValorPago(valorPago);
+		setDataVencimento(dataVencimento);
+		setDataPagamento(dataPagamento);
+	}
+	
+	private void setValorPago(Double valorPago) {
+		this.valorPago = valorPago;
+	}
+	
+	private void setDataVencimento(Data dataVencimento) {
+		this.dataVencimento = dataVencimento;
+	}
+	
+	private void setDataPagamento(Data dataPagamento) {
+		this.dataPagamento= dataPagamento;
+	}
+	
+	public Double getValorPago() {
+		return valorPago;
+	}
+	
+	public Data getDataVencimento() {
+		return dataVencimento;
+	}
+	
+	public Data getDataPagamento() {
+		return dataPagamento;
+	}
 }
