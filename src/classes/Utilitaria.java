11af1a701257a2cad550de8a2e6d59c29625e5ad
GuilhermeAlmeida10000 <guilherme_almeida@ufms.br>
2023-10-25 14:16:29 -0300

    Build: continua a implementação da classe Utilitária.

--- src/classes/Utilitaria.java
+++ src/classes/Utilitaria.java
@@ -12,5 +12,24 @@ public class Utilitaria {
 		return 50 <= numeroDeConvidados && numeroDeConvidados <= 180;
 		
 	}
+	
+	public static boolean verificarValidezDoHorario(String horaDeInicio) {
+		String [] horario = horaDeInicio.split(".");
+		String hora = horario[0];
+		String min = horario[1];
+		int horaok = Integer.valueOf(hora);
+		
+		return 11 <= horaok && horaok <= 20;
+	}
+	
+	public static boolean verificarOpcoesSalgados (<ArrayList>Salgado opcoesDeSalgado){
+		salgadoqtdarray = opcoesDeSalgado.size();
+		
+		return salgadoqtdarray 
+	}
+	
+	public static boolean verificarOpcoesDoces (<ArrayList>Doce opcoesDeDoce){
+		return 
+	}
 
 }
