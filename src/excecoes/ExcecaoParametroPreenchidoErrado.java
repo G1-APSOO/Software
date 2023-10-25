e67e0fed4279afdfb5e60f4191fc2c4cde5ccad9
Matheus <46912481+Mat780@users.noreply.github.com>
2023-10-25 16:27:08 -0400

    fix: Adiciona serialfield para ExcecaoParametroPreenchidoErrado

--- src/excecoes/ExcecaoParametroPreenchidoErrado.java
+++ src/excecoes/ExcecaoParametroPreenchidoErrado.java
@@ -2,7 +2,8 @@ package excecoes;
 
 public class ExcecaoParametroPreenchidoErrado extends RuntimeException {
 	
-	
+	private static final long serialVersionUID = 4386200303229996836L;
+
 	public ExcecaoParametroPreenchidoErrado(String msg){
 		super("O atributo "+ msg + " está preenchido incorretamente.");
 	}
