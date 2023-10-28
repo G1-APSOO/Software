f688b5d7861807fee19401539f7e65202521e8aa
pdrzan <pedeozanelato19@gmail.com>
2023-10-28 19:40:52 -0400

    build: existeEssaChavePrimaria implementado

--- src/bancoDeDados/DAO.java
+++ src/bancoDeDados/DAO.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 public interface DAO<T, PK> {
 	
 	public T get(PK chavePrimaria);
+	public boolean existeEssaChavePrimaria(PK chavePrimaria);
 	public ArrayList<T> getAll();
 	public boolean criar(T objeto);
 	public boolean atualizar(T objeto);
