f851e29b6d437201c77d2436664fd860cfeacfad
Matheus <46912481+Mat780@users.noreply.github.com>
2023-10-30 10:19:33 -0400

    fix: Corrige ControladoraJanela para trocaPainel
    E deixa de guardar um usuario dentro dela

--- src/controladoras/ControladoraJanela.java
+++ src/controladoras/ControladoraJanela.java
@@ -2,31 +2,24 @@ package controladoras;
 
 import javax.swing.JPanel;
 
-import classes.Usuario;
 import telas.JanelaPrincipal;
 import telas.Painel;
 import telas.PainelEscolhaDeOrcamento;
 
 public class ControladoraJanela {
-	private static Usuario usuario = null;
 	private static Painel painelInicial = new PainelEscolhaDeOrcamento(); // Atualmente o inicio é em EscolhaOrcamento
 	private static JanelaPrincipal janela = new JanelaPrincipal();
 	
 	private ControladoraJanela() {} // Controladora vazia, pois é uma classe estatica
 	
 	public static void trocaPainel(JPanel painel) {
-		janela.setContentPane(painel);
+		janela.trocaPainel(painel);
+		
 	}
 	
 	public static void voltarPainelInicial() {
-		janela.setContentPane(painelInicial.getPainel());
-	}
-	
-	public static void setUsuario(Usuario novoUsuario) {
-		if (usuario != null)
-			throw new IllegalAccessError("Acesso negado: Usuário já definido");
-		
-		usuario = novoUsuario;
+		janela.trocaPainel(painelInicial.getPainel());
+
 	}
 	
 }
