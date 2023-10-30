de57e244ae8a590a908932e92b9605f3c9a7618d
Matheus <46912481+Mat780@users.noreply.github.com>
2023-10-30 10:15:07 -0400

    build: Cria classe PopUpErroGenerico

---
+++ src/telas/PopUpErroGenerico.java
@@ -0,0 +1,26 @@
+package telas;
+
+import javax.swing.JPanel;
+
+public class PopUpErroGenerico extends Painel {
+
+	private JPanel painel;
+	
+	public PopUpErroGenerico() {
+		painel = new JPanel();
+		
+	}
+
+	@Override
+	public JPanel getPainel() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected void configurarBotoes() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
