ac1d194f1d722ecee358ac797ebdfa57a1ab7a91
pdrzanFilho <contatesteparalogins@gmail.com>
2023-11-09 15:11:07 -0400

    build: Initial implementation ServicoAdicionalDAO

---
+++ src/bancoDeDados/ServicoAdicionalDAO.java
@@ -0,0 +1,44 @@
+package bancoDeDados;
+
+import java.util.ArrayList;
+import classes.ServicoAdicional;
+
+public class ServicoAdicionalDAO implements DAO<ServicoAdicional, Integer>{
+
+	@Override
+	public ServicoAdicional get(Integer chavePrimaria) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean existeEssaChavePrimaria(Integer chavePrimaria) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public ArrayList<ServicoAdicional> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean criar(ServicoAdicional objeto) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean atualizar(ServicoAdicional objeto) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deletar(Integer chavePrimaria) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
