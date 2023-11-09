4cabbff3d3e92b7de85beaa635eb7fa7a0a0688e
pdrzanFilho <contatesteparalogins@gmail.com>
2023-11-09 15:11:18 -0400

    build: Initial implementation ServicoContratadoDAO

---
+++ src/bancoDeDados/ServicoContratadoDAO.java
@@ -0,0 +1,44 @@
+package bancoDeDados;
+
+import java.util.ArrayList;
+import classes.ServicoContratado;
+
+public class ServicoContratadoDAO implements DAO<ServicoContratado, String>{
+
+	@Override
+	public ServicoContratado get(String chavePrimaria) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean existeEssaChavePrimaria(String chavePrimaria) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public ArrayList<ServicoContratado> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean criar(ServicoContratado objeto) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean atualizar(ServicoContratado objeto) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deletar(String chavePrimaria) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
