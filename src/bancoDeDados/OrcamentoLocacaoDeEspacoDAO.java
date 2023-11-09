c4a133e995d91c6ce876d55d8703afa6e7a5c689
pdrzanFilho <contatesteparalogins@gmail.com>
2023-11-09 15:10:37 -0400

    build: Initial implementation OrcamentoLocacaoDeEspacoDAO

---
+++ src/bancoDeDados/OrcamentoLocacaoDeEspacoDAO.java
@@ -0,0 +1,44 @@
+package bancoDeDados;
+
+import java.util.ArrayList;
+import classes.OrcamentoLocacaoDeEspaco;
+
+public class OrcamentoLocacaoDeEspacoDAO implements DAO<OrcamentoLocacaoDeEspaco, Integer>{
+
+	@Override
+	public OrcamentoLocacaoDeEspaco get(Integer chavePrimaria) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean existeEssaChavePrimaria(Integer chavePrimaria) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public ArrayList<OrcamentoLocacaoDeEspaco> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean criar(OrcamentoLocacaoDeEspaco objeto) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean atualizar(OrcamentoLocacaoDeEspaco objeto) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deletar(Integer chavePrimaria) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+}
