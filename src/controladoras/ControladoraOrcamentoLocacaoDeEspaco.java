0f1f09a5028665db7da091e020876158b67d7d85
Matheus <46912481+Mat780@users.noreply.github.com>
2023-11-19 18:37:54 -0400

    build: Implementa a controladora
    Co-authored-by: Giovanna <giovanna223377@gmail.com>
    Co-authored-by: Guilherme Almeida <guilherme_almeida@ufms.br>

---
+++ src/controladoras/ControladoraOrcamentoLocacaoDeEspaco.java
@@ -0,0 +1,30 @@
+package controladoras;
+
+import negocio.OrcamentoLocacaoDeEspaco;
+import negocio.ServicoContratado;
+import negocio.Cliente;
+import negocio.Data;
+
+import java.util.ArrayList;
+
+public class ControladoraOrcamentoLocacaoDeEspaco {
+
+    private ControladoraOrcamentoLocacaoDeEspaco() throws IllegalAccessException {
+		throw new IllegalAccessException("Instanciamento inválido, esta classe é estática");
+	}
+
+    public static OrcamentoLocacaoDeEspaco criarOrcamento (int numeroDeConvidados, int numeroDeColaboradores, String horaDeInicio, 
+            Data data, Cliente cliente) {
+        return new OrcamentoLocacaoDeEspaco(numeroDeConvidados, numeroDeColaboradores, horaDeInicio, data, cliente);
+    }
+
+    public static void anexarServicos (OrcamentoLocacaoDeEspaco orcamento, ArrayList<ServicoContratado> servicosContratados) {
+        for (int i = 0; i < servicosContratados.size(); i++) {
+            orcamento.anexarServico(servicosContratados.get(i));
+        }
+    }
+
+    public static boolean cadastrarOrcamento (OrcamentoLocacaoDeEspaco orcamento) {
+        return orcamento.cadastrarOrcamento();
+    }
+}
